ab3ad43eb983954363ed926f334d98f342bd9e6a
Lennart van den Dool <lennartvdd@gmail.com>
2024-02-29 11:34:25 +0100

    fix typo

--- addons/stock_account/models/res_company.py
+++ addons/stock_account/models/res_company.py
@@ -9,6 +9,6 @@ class Company(models.Model):
 
     inventory_revaluation_distribution_method = fields.Selection([("quantity","Proportionate to quantity"),("value","Proportionate to value")],
                                                        "Inventory Revaluation Distribution Method",
-                                                       help="Determines the method to distribute revalations over existing values. It can be either proportionate to quantity (default) or value.",
+                                                       help="Determines the method to distribute revaluations over existing values. It can be either proportionate to quantity (default) or proportionate to value.",
                                                        required=True,
                                                        default='quantity')
